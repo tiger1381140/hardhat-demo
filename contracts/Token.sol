cebf0b7fff87726e653f7c16fb828cf0228dbe0a
tiger <tiger13811402285@gmail.com>
2025-05-02 16:10:18 +0800

    add some contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,29 +11,14 @@
 
 pragma solidity ^0.8.28;
 
+import "./IERC20.sol";
+
 address constant _deadAddr = address(0xdead);
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
-}
-
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 library SafeMath {
